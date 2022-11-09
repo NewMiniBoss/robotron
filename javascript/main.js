6ed471aa707bf7a0d11302d3c79d4ef0dbd2b79f
NewMiniBoss <gabriel.f.lazari@gmail.com>
2022-11-09 13:52:11 -0300

    desafio 2

--- javascript/main.js
+++ javascript/main.js
@@ -1,7 +1,13 @@
 const robotron = document.querySelector('[data-robo="tron"]');
 
-robotron.addEventListener('click', () => {
+robotron.addEventListener('click', (evento) => {
     console.log('click');
+    console.log(evento);
 });
 
+function dizOi(nome) {
+    console.log('Oi ' + nome);
+    console.log('Bem vindo ao robotron');
+}
 
+dizOi("Gabriel");

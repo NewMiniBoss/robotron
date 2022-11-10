e04d9c4ceafcefd1b24669be7b40cd89a02fbdd8
NewMiniBoss <gabriel.f.lazari@gmail.com>
2022-11-09 21:40:00 -0300

    desafio hidden

--- javascript/main.js
+++ javascript/main.js
@@ -1,12 +1,13 @@
-const controle = document.querySelectorAll('.controle-ajuste');
-controle.forEach((elemento) => {
+const controle = document.querySelectorAll('[data-controle]');
+controle.forEach(elemento => {
     elemento.addEventListener('click', (evento) => {
-        manipulacao(evento.target.textContent, evento.target.parentNode);
+        manipulacao(evento.target.dataset.controle, evento.target.parentNode);
+        console.log(evento.target.parentNode)
     })
 })
 
 function manipulacao(operacao, controle) {
-    const peca = controle.querySelector('.controle-contador');
+    const peca = controle.querySelector('[data-contador]');
 
     if (operacao === "-") {
         peca.value = parseInt(peca.value) - 1;

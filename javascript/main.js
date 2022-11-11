98cc338a537da21c6ccb2ee0e116522c70d2fa2c
NewMiniBoss <gabriel.f.lazari@gmail.com>
2022-11-11 11:13:40 -0300

    arrays && forEach

--- javascript/main.js
+++ javascript/main.js
@@ -1,16 +1,55 @@
 const controle = document.querySelectorAll('[data-controle]');
+const pecas = {
+    "bracos": {
+        "forca": 29,
+        "poder": 35,
+        "energia": -21,
+        "velocidade": -5
+    },
+
+    "blindagem": {
+        "forca": 41,
+        "poder": 20,
+        "energia": 0,
+        "velocidade": -20
+    },
+    "nucleos": {
+        "forca": 0,
+        "poder": 7,
+        "energia": 48,
+        "velocidade": -24
+    },
+    "pernas": {
+        "forca": 27,
+        "poder": 21,
+        "energia": -32,
+        "velocidade": 42
+    },
+    "foguetes": {
+        "forca": 0,
+        "poder": 28,
+        "energia": 0,
+        "velocidade": -2
+    }
+}
+const estatisticas = document.querySelectorAll('[data-estatistica]');
+
 controle.forEach(elemento => {
     elemento.addEventListener('click', (evento) => {
         manipulacao(evento.target.dataset.controle, evento.target.parentNode);
+        atualizaEstatistica(evento.target.dataset.pecas);
     })
 })
 
 function manipulacao(operacao, controle) {
-    const peca = controle.querySelector('[data-contador]');
-
+    const pecaContador = controle.querySelector('[data-contador]');
     if (operacao === "-") {
-        peca.value = parseInt(peca.value) - 1;
+        pecaContador.value = parseInt(pecaContador.value) - 1;
     } else {
-        peca.value = parseInt(peca.value) + 1;
+        pecaContador.value = parseInt(pecaContador.value) + 1;
     }
+}
+
+function atualizaEstatistica(peca) {
+    console.log(pecas[peca])
 }
